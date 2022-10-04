c6418058ee7b206b4125db1151d1c9bfb6e21478
Andrea Mariana Urrea Urias <andrea.urrea@neoris.com>
2022-10-03 20:01:38 -0600

    added pom

--- src/main/java/SingleResp/UserController.java
+++ src/main/java/SingleResp/UserController.java
@@ -1,5 +1,24 @@
 package SingleResp;
 
-public class UserController {
+import java.io.IOException;
 
+public class UserController {
+    private Store store = new Store() ;
+    // Create a new user
+    public String createUser (String userJson) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        User user = mapper.readValue ( userJson , User.class ) ;
+    }
+   if ( ! isValidUser ( user ) ) {
+        return " ERROR " ;
+    }
+    store.store ( user ) ;
+    return " SUCCESS " ;
+    // Validates the user object
+    private boolean isValidUser ( User user ) {
+        if ( ! isPresent ( user.getName ( ) ) ) {
+            return false ;
+        }
+        user.setName ( user.getName ( ) . trim ( ) ) ;
+        if ( ! isValidAlphaNumeric ( user.getName ( ) ) ) {
 }
